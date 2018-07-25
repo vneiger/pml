1c45de6839fa711ac8e978347c546af853e662f4
Kevin Hyun <ke456@hotmail.com>
2018-07-25 15:11:02 -0400

    most degree checks done

--- NTL-extras/mat_lzz_pX_extra/mat_lzz_pX_extra.h
+++ NTL-extras/mat_lzz_pX_extra/mat_lzz_pX_extra.h
@@ -17,6 +17,36 @@ void random_mat_zz_pX(Mat<zz_pX>& a, long n, long m, long d);
 long deg(const Mat<zz_pX> & a);
 
 /*------------------------------------------------------------*/
+/* matrix of integers; deg(0) = -1                            */
+/*------------------------------------------------------------*/
+void degree_matrix(Mat<long> &a, const Mat<zz_pX> &b, const Vec<long>& shift=Vec<long>(), const bool row_wise=true);
+
+/*------------------------------------------------------------*/
+/* max degree of row entries                                  */
+/*------------------------------------------------------------*/
+void row_degree(Vec<long> &a, const Mat<zz_pX> &b, const Vec<long>& shift=Vec<long>()); 
+
+/*------------------------------------------------------------*/
+/* max degree of col entries                                  */
+/*------------------------------------------------------------*/
+void col_degree(Vec<long> &a, const Mat<zz_pX> &b,const Vec<long>& shift=Vec<long>()); 
+
+/*------------------------------------------------------------*/
+/* leading matrix of b                                        */
+/*------------------------------------------------------------*/
+void leading_matrix(Mat<zz_p> &a, const Mat<zz_pX> &b,const Vec<long> & shift = Vec<long>(), const bool row_wise = true);
+
+/*------------------------------------------------------------*/
+/* returns true if b is reduced                               */
+/*------------------------------------------------------------*/
+bool is_reduced (const Mat<zz_pX> &b,const Vec<long> & shift = Vec<long>(), const bool row_wise = true);
+
+/*------------------------------------------------------------*/
+/* finds the pivot indices; returns the row/col degs          */
+/*------------------------------------------------------------*/
+Vec<long> pivot_index (Vec<long> &index, const Mat<zz_pX> &b,const Vec<long> & shift = Vec<long>(), const bool row_wise = true);
+
+/*------------------------------------------------------------*/
 /* c = a*b                                                    */
 /*------------------------------------------------------------*/
 void multiply_waksman(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & b);
@@ -26,3 +56,41 @@ void multiply_evaluate_FFT(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX
 void multiply_evaluate(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & b);
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- NTL-extras/mat_lzz_pX_extra/src/mat_lzz_pX_extra.cpp
+++ NTL-extras/mat_lzz_pX_extra/src/mat_lzz_pX_extra.cpp
@@ -231,3 +231,224 @@ void multiply_evaluate(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & 
 	multiply_evaluate_geometric(c, a, b);
     }
 }
+
+long min (const Vec<long> &v){
+	if (v.length() == 0) return 0;
+	long m = v[0];
+	for (long i = 1; i < v.length(); i++)
+		if (v[i] < m) m = v[i];
+	return m;
+}
+
+long max(const Vec<long> &v){
+	if (v.length() == 0) return 0;
+	long m = v[0];
+	for (long i = 1; i < v.length(); i++)
+		if (v[i] > m) m = v[i];
+	return m;
+}
+
+void check_shift(bool &shifted, const Vec<long> &shift, const Mat<zz_pX> &b, const bool row_wise = true){
+	shifted = false;
+	if (shift.length() != 0) {
+		shifted = true;
+		if (row_wise && shift.length() != b.NumCols())
+			throw "bad row shift dimensions";
+		if (!row_wise && shift.length() != b.NumRows())
+			throw "bad col shift  dimensions";
+	}
+}
+
+void degree_matrix(Mat<long> &a, const Mat<zz_pX> &b, 
+                   const Vec<long> & shift = Vec<long>(),
+                   const bool row_wise = true){
+	// check for shifts
+	bool shifted;
+	check_shift(shifted, shift, b, row_wise);
+	long min_shift = min(shift);
+	
+	a.SetDims(b.NumRows(), b.NumCols());
+	for (long i = 0; i < b.NumRows(); i++){
+		for (long j = 0; j < b.NumCols(); j++){
+			a[i][j] = deg(b[i][j]);
+			if (shifted){
+				if (a[i][j] == -1) a[i][j] += min_shift;
+				else if (row_wise) a[i][j] += shift[j];
+				else a[i][j] += shift[i];
+			}
+		}
+	}
+}
+
+void row_degree(Vec<long> &a, const Mat<zz_pX> &b,
+                const Vec<long> &shift = Vec<long>()){ 
+  bool shifted;
+  check_shift(shifted,shift,b);
+  
+	a.SetLength(b.NumRows());
+	Mat<long> deg_mat;
+	degree_matrix(deg_mat,b,shift,true);
+	
+	for (long r = 0; r < b.NumRows(); r++){
+		auto max_deg = deg_mat[r][0];
+		for (long c = 1; c < b.NumCols(); c++){
+			if (max_deg < deg_mat[r][c]) max_deg = deg_mat[r][c];
+		}
+		a[r] = max_deg;
+	}
+}
+
+void col_degree(Vec<long> &a, const Mat<zz_pX> &b,
+                const Vec<long> &shift = Vec<long>()){
+	bool shifted;
+	check_shift(shifted,shift,b,false);
+	
+	a.SetLength(b.NumCols());
+	Mat<long> deg_mat;
+	degree_matrix(deg_mat,b,shift,false);
+	
+	a.SetLength(b.NumCols());
+	for (long c = 0; c < b.NumCols(); c++){
+		auto max_deg = deg_mat[0][c];
+		for (long r = 1; r < b.NumRows(); r++){
+			if (max_deg < deg_mat[r][c]) max_deg = deg_mat[r][c];
+		}
+		a[c] = max_deg;
+	}
+} 
+
+void leading_matrix(Mat<zz_p> &a, const Mat<zz_pX> &b, 
+                   const Vec<long> & shift = Vec<long>(),
+                   const bool row_wise = true){
+	// check for shifts
+	bool shifted;
+	check_shift(shifted, shift, b, row_wise);
+	
+	Vec<long> degree;
+	if (row_wise)
+		row_degree(degree,b,shift);
+	else
+		col_degree(degree,b,shift);
+	
+	a.SetDims(b.NumRows(), b.NumCols());
+	for (long r = 0; r < b.NumRows(); r++)
+		for (long c = 0; c < b.NumCols(); c++){
+			long d,d2;
+			if (row_wise) d = degree[r];
+			else d = degree[c];
+			d2 = deg(b[r][c]);
+			if (shifted){
+				if (row_wise) d2 += shift[c];
+				else d2 += shift[r];
+			}
+			if (d2 >= d)
+				a[r][c] = LeadCoeff(b[r][c]);
+		}
+}
+
+bool is_reduced (const Mat<zz_pX> &b,const Vec<long> & shift = Vec<long>(), const bool row_wise = true){
+	Mat<zz_p> lead_mat;
+	leading_matrix(lead_mat,b,shift,row_wise);
+	auto rank = gauss(lead_mat);
+	if (row_wise)
+		return b.NumRows() == rank;
+	return b.NumCols() == rank;
+}
+
+Vec<long> pivot_index (Vec<long> &index, const Mat<zz_pX> &b,const Vec<long> & shift = Vec<long>(), const bool row_wise = true){
+	bool shifted;
+	check_shift(shifted, shift, b, row_wise);
+
+	Mat<long> deg_mat;
+	degree_matrix(deg_mat,b,shift,row_wise);
+	
+	Vec<long> degree;
+	if (row_wise) row_degree(degree,b,shift);
+	else col_degree(degree,b,shift);
+	long zero_degree = -1;
+	if (shifted)
+		zero_degree = min(shift) -1;
+	
+	if (row_wise){
+		index.SetLength(b.NumRows());
+		for (long r = 0; r < b.NumRows(); r++){
+			if (degree[r] == zero_degree) index[r] = -1;
+			else
+				for (long c = 0; c <b.NumCols(); c++)
+					if (deg_mat[r][c] == degree[r]) index[r] = c;
+		}
+	}else{
+		index.SetLength(b.NumCols());
+		for(long c = 0; c < b.NumCols(); c++){
+			if (degree[c] == zero_degree) index[c] = -1;
+			else
+				for (long r = 0; r < b.NumRows(); r++)
+					if (deg_mat[r][c] == degree[c]) index[c] = r;
+		}
+	}
+	return degree;
+}
+
+/*
+bool is_weak_popov (const Mat<zz_pX> &m, const Vec<long> &shift = Vec<long>(), const bool row_wise = true){
+	Vec<long> pivots;
+	pivot_index(pivots, b, shift, row_wise);
+	
+}
+
+bool is_popov (const Mat<zz_pX> &m, const Vec<long> &shift = Vec<long>(), const bool row_wise = true){
+	Vec<long> pivots;
+	Vec<long degree = pivot_index(pivots, b, shift, row_wise);
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
