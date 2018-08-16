99c42d6f56fcd5cfde687d15ed35f97e61d18bb3
Eric Schost <eschost@uwaterloo.ca>
2018-08-16 11:36:20 -0400

    new directories -- for consistency

---
+++ NTL-extras/vec_lzz_p_extra/timings/time_inv.cpp
@@ -0,0 +1,67 @@
+#include <NTL/vec_lzz_p.h>
+#include <assert.h>
+
+#include "util.h"
+#include "vec_lzz_p_extra.h"
+
+NTL_CLIENT
+
+/*------------------------------------------------------------*/
+/* runs timings up to size 1000                               */
+/*------------------------------------------------------------*/
+void check(int opt)
+{
+    zz_p::FFTInit(0);
+    long p = zz_p::modulus();
+
+    for (long i = 0; i < 1000; i += 1)
+    {
+        vec_zz_p A, invA1, invA2;
+        random_vec_zz_p(A, i);
+
+        cout << p << " ";
+        cout << i << " ";
+        double t;
+        long nb;
+        const double thresh = 0.05;
+
+        nb = 0;
+        t = get_time();
+        do
+        {
+            inv(invA1, A);
+            nb++;
+        }
+        while ( (get_time() - t) < thresh);
+        t = (get_time() - t) / nb;
+        cout << t << " ";
+
+        nb = 0;
+        t = get_time();
+        do
+        {
+            inv_naive(invA1, A);
+            nb++;
+        }
+        while ( (get_time() - t) < thresh);
+        t = (get_time() - t) / nb;
+        cout << t << " ";
+        
+        cout << endl;
+    }
+}
+
+/*------------------------------------------------------------*/
+/* main just calls check()                                    */
+/* if not argument is given, runs timings                     */
+/* if the argument 1 is given, runs check                     */
+/*------------------------------------------------------------*/
+int main(int argc, char** argv)
+{
+    int opt = 0;
+    if (argc > 1)
+        opt = atoi(argv[1]);
+    check(opt);
+
+    return 0;
+}
