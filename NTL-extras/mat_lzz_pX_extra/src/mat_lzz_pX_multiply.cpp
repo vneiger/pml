53a733e0b0a684f8e10af75a1aae860e0fc4db36
Kevin Hyun <ke456@hotmail.com>
2018-09-14 15:14:39 +0200

    integrated parallel

--- NTL-extras/mat_lzz_pX_extra/src/mat_lzz_pX_multiply.cpp
+++ NTL-extras/mat_lzz_pX_extra/src/mat_lzz_pX_multiply.cpp
@@ -16,102 +16,6 @@ NTL_CLIENT
 /*------------------------------------------------------------*/
 void multiply_evaluate_FFT(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & b)
 {
-    long s = a.NumRows();
-    long t = a.NumCols();
-    long u = b.NumCols();
-
-    long dA = deg(a);
-    long dB = deg(b);
-
-    long idxk = NextPowerOfTwo(dA + dB + 1);
-    fftRep R1(INIT_SIZE, idxk);
-
-    long n = 1 << idxk;
-
-    Vec<zz_p> mat_valA, mat_valB;
-    Vec<Vec<zz_p>> mat_valC;
-
-    mat_valA.SetLength(n * s * t);
-    mat_valB.SetLength(n * t * u);
-
-    Vec<zz_p> tmp;
-    long st = s*t;
-    for (long i = 0; i < s; i++)
-    {
-        for (long k = 0; k < t; k++)
-        {
-            TofftRep(R1, a[i][k], idxk);
-            long *frept = & R1.tbl[0][0];
-            for (long r = 0, rst = 0; r < n; r++, rst += st)
-                mat_valA[rst + i*t + k] = frept[r];
-        }
-    }
-
-    long tu = t*u;
-    for (long i = 0; i < t; i++)
-    {
-        for (long k = 0; k < u; k++)
-        {
-            TofftRep(R1, b[i][k], idxk);
-            long *frept = & R1.tbl[0][0];
-            for (long r = 0, rtu = 0; r < n; r++, rtu += tu)
-                mat_valB[rtu + i*u + k] = frept[r];
-        }
-    }
-
-    Mat<zz_p> va, vb, vc;
-    va.SetDims(s, t);
-    vb.SetDims(t, u);
-
-    mat_valC.SetLength(s * u);
-    for (long i = 0; i < s * u; i++)
-        mat_valC[i].SetLength(n);
-
-    for (long j = 0, jst = 0, jtu = 0; j < n; j++, jst += st, jtu += tu)
-    {
-        for (long i = 0; i < s; i++)
-        {
-            for (long k = 0; k < t; k++)
-            {
-                va[i][k] = mat_valA[jst + i*t + k];
-            }
-        }
-        for (long i = 0; i < t; i++)
-        {
-            for (long k = 0; k < u; k++)
-            {
-                vb[i][k] = mat_valB[jtu + i*u + k];
-            }
-        }
-
-        vc = va * vb;
-
-        for (long i = 0; i < s; i++)
-        {
-            for (long k = 0; k < u; k++)
-            {
-                mat_valC[i*u + k][j] = vc[i][k];
-            }
-        }
-    }
-
-    c.SetDims(s, u);
-    for (long i = 0; i < s; i++)
-    {
-        for (long k = 0; k < u; k++)
-        {
-            long *frept = & R1.tbl[0][0];
-            for (long r = 0; r < n; r++)
-            {
-                frept[r] = rep(mat_valC[i*u + k][r]);
-            }
-            FromfftRep(c[i][k], R1, 0, n - 1);
-        }
-    }
-}
-
-void multiply_evaluate_FFT_parallel(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & b)
-{
     zz_pContext context;
     long s = a.NumRows();
     long t = a.NumCols();
@@ -226,6 +130,8 @@ NTL_EXEC_RANGE(n,first,last)
 NTL_EXEC_RANGE_END
 /*** END PARALLEL **********************************************/
 
+    c.SetDims(s, u);
+
 /*** START PARALLEL ********************************************/ 
     context.save();  
 
@@ -234,7 +140,7 @@ NTL_EXEC_RANGE(s,first,last)
     context.restore();
     
     Mat<zz_p> vc; 
-    c.SetDims(s, u);
+    
     
     fftRep R = R1;
     
@@ -264,6 +170,7 @@ NTL_EXEC_RANGE_END
 /*------------------------------------------------------------*/
 void multiply_evaluate(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & b)
 {
+    
     if (is_FFT_ready(NextPowerOfTwo(deg(a) + deg(b) + 1)))
     {
         multiply_evaluate_FFT(c, a, b);
@@ -272,23 +179,7 @@ void multiply_evaluate(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & 
     {
         multiply_evaluate_geometric(c, a, b);
     }
-
-    //multiply_evaluate_geometric(c, a, b);
 }
-
-void multiply_evaluate_parallel(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & b)
-{
-    if (is_FFT_ready(NextPowerOfTwo(deg(a) + deg(b) + 1)))
-    {
-        multiply_evaluate_FFT_parallel(c, a, b);
-    }
-    else
-    {
-        multiply_evaluate_geometric_parallel(c, a, b);
-    }
-    //multiply_evaluate_geometric_parallel(c, a, b);
-}
-
 
 /*------------------------------------------------------------*/
 /* c = a*b                                                    */
--- NTL-extras/mat_lzz_pX_extra/timings/time_one_multiply.cpp
+++ NTL-extras/mat_lzz_pX_extra/timings/time_one_multiply.cpp
@@ -42,54 +42,9 @@ void check(long p, long sz, long deg)
     }
     while ((get_time()-t_eval) <= thres);
     t_eval = (get_time()-t_eval) / nb;
-
-    auto temp_c = c;
-    t_eval_p = get_time();
-    nb = 0;
-    do
-    {
-        multiply_evaluate_parallel(temp_c, a, b);
-        nb++;
-    }
-    while ((get_time()-t_eval_p) <= thres);
-    t_eval_p = (get_time()-t_eval_p) / nb;
-
-    if (temp_c != c) cout << "BAD!!!!!!!" << endl;
-    /*
-       t_3primes = get_time();
-       nb = 0;
-       do
-       {
-       multiply_3_primes(c, a, b);
-       nb++;
-       }
-       while ((get_time()-t_3primes) <= thres);
-       t_3primes = (get_time()-t_3primes) / nb;
-
-       t_waksman = get_time();
-       nb = 0;
-       do
-       {
-       multiply_waksman(c, a, b);
-       nb++;
-       }
-       while ((get_time()-t_waksman) <= thres);
-       t_waksman = (get_time()-t_waksman) / nb;
-
-
-       t_check = get_time();
-       nb = 0;
-       do
-       {
-       multiply(c, a, b);
-       nb++;
-       }
-       while ((get_time()-t_check) <= thres);
-       t_check = (get_time()-t_check) / nb;
-       */
+    
     cout << p << " " << sz << " " << deg << endl;
-    cout << t_eval << " " << t_eval_p << endl;
-    //cout << t_eval << " " << t_eval_p << " " << t_3primes << " " << t_waksman << " " << t_check << endl;
+    cout << t_eval << << endl;
 }
 
 
