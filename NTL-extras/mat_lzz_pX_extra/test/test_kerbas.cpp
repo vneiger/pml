ae2224f8b2cb15a20082fe91057a9f148ef1a14e
Vincent Neiger <vincent.neiger@unilim.fr>
2018-10-07 18:40:01 +0200

    remove spaces at EOL

--- NTL-extras/mat_lzz_pX_extra/test/test_kerbas.cpp
+++ NTL-extras/mat_lzz_pX_extra/test/test_kerbas.cpp
@@ -12,27 +12,27 @@ NTL_CLIENT
 void check(long m, long n, long d){
     Mat<zz_pX> pmat;
     random_mat_zz_pX(pmat, m, n, d);
-    
+
     cout << "pmat: " << endl << degree_matrix(pmat) << endl;
-    
+
     Shift s;
     for (long i = 0; i < m; i++)
         s.emplace_back(d+i);
-    
+
     Mat<zz_pX> kerbas;
     kernel_basis(kerbas, pmat, s);
-    
+
     cout << "kerbas (appbas): " << endl << degree_matrix(kerbas) << endl;
-    
+
     Mat<zz_pX> res;
     multiply(res, kerbas, pmat);
     cout << "product sould be zero: " << endl << degree_matrix(res) << endl;
-    
+
     kerbas = Mat<zz_pX>();
     kernel_basis_intbas(kerbas, pmat, s);
-    
+
     cout << "kerbas (intbas): " << endl << degree_matrix(kerbas) << endl;
-    
+
     multiply(res, kerbas, pmat);
     cout << degree_matrix(res) << endl;
 }
@@ -43,14 +43,14 @@ void check(long m, long n, long d){
 int main(int argc, char ** argv)
 {
     //SetNumThreads(4);
-    
+
     zz_p::init(NTL::GenPrime_long(60));
 
     if (argc==4)
     {
         check(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
     }
-    
+
 
     return 0;
 }
