cbd8fe04d0cd622c8c8d659cb4665e9d0265bead
Vincent Neiger <vincent.neiger@unilim.fr>
2018-11-28 11:18:15 +0100

    augmented the comparison matpade - pmbasis - Hankel solver

--- NTL-extras/mat_lzz_pX_extra/timings/time_compare.cpp
+++ NTL-extras/mat_lzz_pX_extra/timings/time_compare.cpp
@@ -19,51 +19,98 @@ NTL_CLIENT
 int main(int argc, char *argv[])
 {
     if (argc != 3) throw std::invalid_argument("usage: ./time_compare m d");
+
     zz_p::FFTInit(0);
 
     long m = atoi(argv[1]);
     long d = atoi(argv[2]);
 
-    // pmbasis
-    Mat<zz_pX> pmat;
-    random(pmat,2*m, m, d);
-    Mat<zz_pX> appbas;
-    VecLong shift(2*m,0);
+    std::cout << "Comparing:" << std::endl;
+    std::cout << "-- computing denominator P such that deg(PF) < d, where F random m x m matrix of degree < 2d (done with matrix Pade generic code: timings include checking the result is correct)" << std::endl;
+    std::cout << "-- computing approximant basis of [[F], [-1]] at order 2d, with F random m x m matrix of degree < 2d" << std::endl;
+    std::cout << "-- solving a block Hankel system with m x m random Hankel blocks, each d x d" << std::endl;
+    std::cout << "-- m = " << m << ", d = " << d << std::endl;
 
-    double t = GetWallTime();
-    pmbasis(appbas, pmat, d+1, shift);
-    cout << "time pmbasis: " << GetWallTime() - t << endl;
+    double t, tt;
+    long nb_iter;
+    bool matpade = true;
+
+    // generic matrix Pade
+    nb_iter = 0; t = 0.0;
+    while (t<0.5 && matpade)
+    {
+        // set pmat random m x m of degree < 2d
+        Mat<zz_pX> pmat;
+        random(pmat, m, m, 2*d);
+
+        // compute fraction denominator
+        tt = GetWallTime();
+        Mat<zz_pX> denom;
+        matrix_pade_generic(denom, pmat, 2*d);
+        Mat<zz_pX> prod;
+        mul_trunc(prod, denom, pmat, 2*d);
+        if ((deg(denom) > d) || deg(prod) >= d)
+            matpade = false;
+        t += GetWallTime() - tt;
+        ++nb_iter;
+    }
+    cout << "matrix Pade:\t" << t/nb_iter << (matpade ? " (correct)" : " (error)" ) << endl;
+
+    // pmbasis
+    nb_iter = 0; t = 0.0;
+    while (t<0.5)
+    {
+        // set pmat to [[F], [-1]] with F random m x m of degree < 2d
+        Mat<zz_pX> pmat;
+        random(pmat, m, m, 2*d);
+        pmat.SetDims(2*m, m);
+        for (long i = 0; i < m; ++i)
+            pmat[i+m][i] = -1;
+
+        // compute approx basis
+        tt = GetWallTime();
+        VecLong shift(2*m,0);
+        Mat<zz_pX> appbas;
+        pmbasis(appbas, pmat, 2*d, shift);
+        t += GetWallTime() - tt;
+        ++nb_iter;
+    }
+    cout << "pmbasis:\t" << t/nb_iter << endl;
 
     // structured matrix
-    long len = 2*d-1; //num of elements needed dxd blocks
-    Vec<Vec<hankel_lzz_p>> h_vec; //set to m blocks by m blocks
-    h_vec.SetLength(m);
-    for (long i = 0; i < m ; i++)
+    nb_iter = 0; t = 0.0;
+    while (t<0.5)
     {
-        h_vec[i].SetLength(m);
-    }
-    
-    for (long i = 0; i < h_vec.length(); i++)
-    {
-        Vec<zz_p> rvec;
-        rvec.SetLength(len);
-        for (long j = 0; j < h_vec[0].length(); j++)
-        {
-            for (long t = 0; t < len; t++)
-                random(rvec[t]);
-            h_vec[i][j] = hankel_lzz_p(rvec,d,d);
-        }
-    }
-    mosaic_hankel_lzz_p mh{h_vec};
-    Vec<zz_p> b;
-    b.SetLength(m*d);
-    for (long i = 0; i < m*d; i++)
-        random(b[i]);
+        Vec<Vec<hankel_lzz_p>> h_vec; //set to m blocks by m blocks
+        h_vec.SetLength(m);
+        for (long i = 0; i < m ; i++)
+            h_vec[i].SetLength(m);
 
-    Vec<zz_p> x;
-    t = GetWallTime();
-    mh.solve(x,b);
-    cout << "time mh solve: " << GetWallTime() - t << endl;
-    
+        for (long i = 0; i < m; i++)
+            for (long j = 0; j < m; j++)
+                h_vec[i][j] = hankel_lzz_p(random_vec_zz_p(2*d-1),d,d);
+
+        mosaic_hankel_lzz_p mh{h_vec};
+
+        Vec<zz_p> b;
+        random(b, m*d);
+
+        tt = GetWallTime();
+        Vec<zz_p> x;
+        mh.solve(x,b);
+        t += GetWallTime() - tt;
+
+        ++nb_iter;
+    }
+
+    cout << "block Hankel:\t" << t/nb_iter << endl;
 }
 
+
+// Local Variables:
+// mode: C++
+// tab-width: 4
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// End:
+// vim:sts=4:sw=4:ts=4:et:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
