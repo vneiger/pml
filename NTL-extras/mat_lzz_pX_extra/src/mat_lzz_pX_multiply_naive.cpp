c8eef1ac424ef4583d9ce2313f5958049dfa4be7
Vincent Neiger <vincent.neiger@unilim.fr>
2018-12-12 00:59:05 +0100

    reviewing waksman: cleaning and small speedup

--- NTL-extras/mat_lzz_pX_extra/src/mat_lzz_pX_multiply_naive.cpp
+++ NTL-extras/mat_lzz_pX_extra/src/mat_lzz_pX_multiply_naive.cpp
@@ -25,41 +25,93 @@ void multiply_waksman(Mat<zz_pX> &c, const Mat<zz_pX> &a, const Mat<zz_pX> &b)
     }
 
     Vec<zz_pX> d, e;
-    zz_pX val0, val1;
+    zz_pX buf0, buf1, buf2, buf3, buf4;
 
     const long m = a.NumRows();
     const long n = a.NumCols();
     const long p = b.NumCols();
 
     c.SetDims(m, p);
-    clear(c);
 
     d.SetLength(p);
     e.SetLength(m);
 
-    for (long j=1; j<=n/2; ++j)
+    // first iteration j==1 (and j2==1)
+    // --> sets initial value for each entry of c
+    // (thus, no need to clear its values even if nonzero in input)
+    for (long k=0; k<p; ++k)
+    {
+        // c[0][k] += (a[0][j2-1]+b[j2][k]) * (a[0][j2]+b[j2-1][k]);
+        // d[k] += (a[0][j2-1]-b[j2][k]) * (a[0][j2]-b[j2-1][k]);
+        add(buf0, a[0][0], b[1][k]);
+        sub(buf2, a[0][0], b[1][k]);
+        add(buf1, a[0][1], b[0][k]);
+        sub(buf3, a[0][1], b[0][k]);
+        mul(c[0][k], buf0, buf1);
+        mul(d[k], buf2, buf3);
+    }
+
+    for (long l=1; l<m; ++l)
+    {
+        // c[l][0] += (a[l][j2-1]+b[j2][0]) * (a[l][j2]+b[j2-1][0]);
+        // e[l] += (a[l][j2-1]-b[j2][0]) * (a[l][j2]-b[j2-1][0]);
+        add(buf0, a[l][0], b[1][0]);
+        sub(buf2, a[l][0], b[1][0]);
+        add(buf1, a[l][1], b[0][0]);
+        sub(buf3, a[l][1], b[0][0]);
+        mul(c[l][0], buf0, buf1);
+        mul(e[l], buf2, buf3);
+    }
+
+    for (long k=1; k<p; ++k)
+        for (long l=1; l<m; ++l)
+        {
+            add(buf0, a[l][0], b[1][k]);
+            add(buf1, a[l][1], b[0][k]);
+            mul(c[l][k], buf0, buf1);
+        }
+
+    // other iterations, j>1
+    for (long j=2; j<=n/2; ++j)
     {
         const long j2=2*j-1;
 
-        for (long k=0; k<p; k++)
+        for (long k=0; k<p; ++k)
         {
-            c[0][k] += (a[0][j2-1]+b[j2][k]) * (a[0][j2]+b[j2-1][k]);
-            d[k] += (a[0][j2-1]-b[j2][k]) * (a[0][j2]-b[j2-1][k]);
+            // c[0][k] += (a[0][j2-1]+b[j2][k]) * (a[0][j2]+b[j2-1][k]);
+            // d[k] += (a[0][j2-1]-b[j2][k]) * (a[0][j2]-b[j2-1][k]);
+            add(buf0, a[0][j2-1], b[j2][k]);
+            sub(buf2, a[0][j2-1], b[j2][k]);
+            add(buf1, a[0][j2], b[j2-1][k]);
+            sub(buf3, a[0][j2], b[j2-1][k]);
+            mul(buf4, buf0, buf1);
+            add(c[0][k], c[0][k], buf4);
+            mul(buf4, buf2, buf3);
+            add(d[k], d[k], buf4);
         }
 
-        for (long l=1; l<m; l++)
+        for (long l=1; l<m; ++l)
         {
-            c[l][0] += (a[l][j2-1]+b[j2][0]) * (a[l][j2]+b[j2-1][0]);
-            e[l] += (a[l][j2-1]-b[j2][0]) * (a[l][j2]-b[j2-1][0]);
+            // c[l][0] += (a[l][j2-1]+b[j2][0]) * (a[l][j2]+b[j2-1][0]);
+            // e[l] += (a[l][j2-1]-b[j2][0]) * (a[l][j2]-b[j2-1][0]);
+            add(buf0, a[l][j2-1], b[j2][0]);
+            sub(buf2, a[l][j2-1], b[j2][0]);
+            add(buf1, a[l][j2], b[j2-1][0]);
+            sub(buf3, a[l][j2], b[j2-1][0]);
+            mul(buf4, buf0, buf1);
+            add(c[l][0], c[l][0], buf4);
+            mul(buf4, buf2, buf3);
+            add(e[l], e[l], buf4);
         }
 
-        for (long k=1; k<p; k++)
-        {
-            for (long l=1; l<m; l++)
+        for (long k=1; k<p; ++k)
+            for (long l=1; l<m; ++l)
             {
-                c[l][k] += (a[l][j2-1]+b[j2][k]) * (a[l][j2]+b[j2-1][k]);
+                add(buf0, a[l][j2-1], b[j2][k]);
+                add(buf1, a[l][j2], b[j2-1][k]);
+                mul(buf2, buf0, buf1);
+                add(c[l][k], c[l][k], buf2);
             }
-        }
     }
 
     for (long l=1; l<m; l++)
@@ -68,17 +120,15 @@ void multiply_waksman(Mat<zz_pX> &c, const Mat<zz_pX> &a, const Mat<zz_pX> &b)
         c[l][0] -= e[l];
     }
 
-    val0 = (d[0]+c[0][0])/2;
-    c[0][0] -= val0;
+    buf0 = (d[0]+c[0][0])/2;
+    c[0][0] -= buf0;
     for (long k=1; k<p; k++)
     {
-        val1 = (d[k]+c[0][k])/2;
-        c[0][k] -= val1;
-        val1 -= val0;
+        buf1 = (d[k]+c[0][k])/2;
+        c[0][k] -= buf1;
+        buf1 -= buf0;
         for (long l=1; l<m; l++)
-        {
-            c[l][k] -= val1 + e[l];
-        }
+            c[l][k] -= buf1 + e[l];
     }
 
     if ( (n&1) == 1)
