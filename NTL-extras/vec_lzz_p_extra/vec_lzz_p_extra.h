77827272768113bf3d4511032f45d8763aae41a8
Vincent Neiger <vincent.neiger@unilim.fr>
2018-12-18 19:03:16 +0100

    remove functions random for vectors and matrices, already in NTL

--- NTL-extras/vec_lzz_p_extra/vec_lzz_p_extra.h
+++ NTL-extras/vec_lzz_p_extra/vec_lzz_p_extra.h
@@ -1,38 +1,29 @@
 #ifndef __VEC_LZZ_P_EXTRA_H
 #define __VEC_LZZ_P_EXTRA_H
 
+/** Additional functions for `vec_lzz_p`.
+ *
+ * \file vec_lzz_p_extra.h
+ * \author Seung Gyu Hyun, Vincent Neiger, Eric Schost
+ * \version 0.1
+ * \date 2018-12-18
+ *
+ */
+
 #include <NTL/mat_lzz_p.h>
 #include <NTL/vec_lzz_p.h>
 #include <NTL/version.h>
 
 NTL_CLIENT
 
-/*------------------------------------------------------------*/
-/* random vector of length d                                  */
-/*------------------------------------------------------------*/
-void random_vec_zz_p(Vec<zz_p>& A, long d);
-
 #if ( (NTL_MAJOR_VERSION < 10) || ((NTL_MAJOR_VERSION == 10) && (NTL_MINOR_VERSION < 4)) )
 inline Vec<zz_p> random_vec_zz_p(long n)
-{ 
-    Vec<zz_p> x; 
-    random_vec_zz_p(x, n); 
-    return x;
-}
+{ Vec<zz_p> x; random(x, n); return x; }
 #endif
-
-/*------------------------------------------------------------*/
-/* random matrix of size (d,e)                                */
-/*------------------------------------------------------------*/
-void random_mat_zz_p(mat_zz_p& A, long d, long e);
 
 #if ( (NTL_MAJOR_VERSION < 10) || ((NTL_MAJOR_VERSION == 10) && (NTL_MINOR_VERSION < 4)) )
 inline Mat<zz_p> random_mat_zz_p(long d, long e)
-{ 
-    Mat<zz_p> x; 
-    random_mat_zz_p(x, d, e); 
-    return x;
-}
+{ Mat<zz_p> x; random(x, d, e); return x; }
 #endif
 
 /*------------------------------------------------------------*/
