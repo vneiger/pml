0d9042ac477cf12f26bd52519a891ee91c71d0e7
Vincent Neiger <vincent.neiger@unilim.fr>
2019-01-24 15:56:23 +0100

    more complete ntlx/linbox comparison

--- NTL-extras/mat_lzz_pX_extra/timings/ntlx_linbox.cpp
+++ NTL-extras/mat_lzz_pX_extra/timings/ntlx_linbox.cpp
@@ -87,13 +87,14 @@ void one_bench_multiply(long sz, long deg, Field field)
 }
 
 /*------------------------------------------------------------*/
-/* compare approximant basis                                  */
+/* compare divide and conquer approximant basis               */
 /* (2n x n matrix, uniform order, uniform shift)              */
 /*------------------------------------------------------------*/
 template <typename Field>
 void one_bench_pmbasis(long n, long order, Field field)
 {
-    double t_ntlx,t_linbox,t_ntlx_generic;
+    //double t_ntlx,t_linbox,t_ntlx_generic;
+    double t_ntlx,t_linbox;
     long nb_iter;
     double t;
 
@@ -106,7 +107,8 @@ void one_bench_pmbasis(long n, long order, Field field)
 
         t = GetWallTime();
         Mat<zz_pX> appbas;
-        pmbasis(appbas, mat, order, shift_ntl);
+        VecLong rdeg(shift_ntl);
+        pmbasis(appbas, mat, order, rdeg);
         t_ntlx += GetWallTime()-t;
 
         ++nb_iter;
@@ -135,24 +137,78 @@ void one_bench_pmbasis(long n, long order, Field field)
     }
     t_linbox /= nb_iter;
 
-    nb_iter=0; t_ntlx_generic=0.0;
-    while (t_ntlx_generic<0.2)
+    //nb_iter=0; t_ntlx_generic=0.0;
+    //while (t_ntlx_generic<0.2)
+    //{
+    //    Mat<zz_pX> mat;
+    //    random(mat, 2*n, n, order);
+
+    //    t = GetWallTime();
+    //    Mat<zz_pX> appbas;
+    //    pmbasis_generic_2n_n(appbas, mat, order);
+    //    t_ntlx_generic += GetWallTime()-t;
+
+    //    ++nb_iter;
+    //}
+    //t_ntlx_generic /= nb_iter;
+
+    //cout << 2*n << "\t" << n << "\t" << order << "\t" << t_ntlx << "\t" << t_linbox << "\t" << t_ntlx_generic << "\t" << t_ntlx/t_linbox << "\t" << t_ntlx_generic / t_ntlx << endl;
+    cout << 2*n << "\t" << n << "\t" << order << "\t" << t_ntlx << "\t" << t_linbox << "\t" << t_ntlx/t_linbox << endl;
+}
+
+
+/*------------------------------------------------------------*/
+/* compare iterative approximant basis                        */
+/* (2n x n matrix, uniform order, uniform shift)              */
+/*------------------------------------------------------------*/
+template <typename Field>
+void one_bench_mbasis(long n, long order, Field field)
+{
+    double t_ntlx,t_linbox;
+    long nb_iter;
+    double t;
+
+    nb_iter=0; t_ntlx=0.0;
+    VecLong shift_ntl(2*n,0);
+    while (t_ntlx<0.2)
     {
         Mat<zz_pX> mat;
         random(mat, 2*n, n, order);
 
         t = GetWallTime();
         Mat<zz_pX> appbas;
-        pmbasis_generic_2n_n(appbas, mat, order);
-        t_ntlx_generic += GetWallTime()-t;
+        VecLong rdeg(shift_ntl);
+        mbasis(appbas, mat, order, rdeg);
+        t_ntlx += GetWallTime()-t;
 
         ++nb_iter;
     }
-    t_ntlx_generic /= nb_iter;
+    t_ntlx /= nb_iter;
 
-    cout << 2*n << "\t" << n << "\t" << order << "\t" << t_ntlx << "\t" << t_linbox << "\t" << t_ntlx_generic << "\t" << t_ntlx/t_linbox << "\t" << t_ntlx_generic / t_ntlx << endl;
+    nb_iter=0; t_linbox=0.0;
+    std::vector<unsigned long> shift_linbox(2*n,0);
+    while (t_linbox<0.2)
+    {
+        LinBox::PolynomialMatrix<LinBox::PMType::polfirst,LinBox::PMStorage::plain,Field>
+        mat(field,2*n,n,order);
+        typename Field::RandIter Gen(field,time(NULL));
+        LinBox::OrderBasis<Field> OB(field);
+        // Generate random matrix of polynomials
+        for (long i=0;i<2*n*n;++i)
+            randomVect(Gen,mat(i));
+
+        t = GetWallTime();
+        LinBox::PolynomialMatrix<LinBox::PMType::polfirst,LinBox::PMStorage::plain,Field>
+        appbas(field,2*n,2*n,order);
+		OB.M_Basis(appbas, mat, order, shift_linbox);
+        t_linbox += GetWallTime() - t;
+
+        ++nb_iter;
+    }
+    t_linbox /= nb_iter;
+
+    cout << 2*n << "\t" << n << "\t" << order << "\t" << t_ntlx << "\t" << t_linbox << "\t" << t_ntlx/t_linbox << endl;
 }
-
 
 /*------------------------------------------------------------*/
 /* run bench on collection of size/degree                     */
@@ -215,15 +271,15 @@ void run_bench(long test, long nbits, bool fftprime)
         cout << "Prime p = " << zz_p::modulus() << "  (bit length = " << nbits << ")" << endl;
     }
 
-    if ((test==0) | (test==1))
+    if ((test==0) | (test==1) )
     {
         std::cout << "Bench polynomial matrix multiplication" << std::endl;
         std::cout << "size\tdegree\tntlx\tlinbox\tratio(ntlx/linbox)" << std::endl;
 
-        if (nbits<26)
+        if (NumBits(zz_p::modulus()) < 26)
         {
             // LinBox initialize field
-            Givaro::Modular<double> field((int32_t)786433);
+            Givaro::Modular<double> field((int32_t)zz_p::modulus());
             for (size_t i=0;i<szs.size();i++)
                 one_bench_multiply(szs[i],degs[i],field);
         }
@@ -240,12 +296,13 @@ void run_bench(long test, long nbits, bool fftprime)
     if ((test==0) | (test==2))
     {
         std::cout << "Bench pmbasis (divide and conquer minimal approximant basis)" << std::endl;
-        std::cout << "rowdim\tcoldim\tdegree\tntlx\tlinbox\tntlxgen\tntl/lb\tntlxgen/ntlx" << std::endl;
+        //std::cout << "rowdim\tcoldim\tdegree\tntlx\tlinbox\tntlxgen\tntl/lb\tntlxgen/ntlx" << std::endl;
+        std::cout << "rowdim\tcoldim\tdegree\tntlx\tlinbox\tntl/lb" << std::endl;
 
-        if (nbits < 26)
+        if (NumBits(zz_p::modulus()) < 26)
         {
             // LinBox initialize field
-            Givaro::Modular<double> field((int32_t)786433);
+            Givaro::Modular<double> field((int32_t)zz_p::modulus());
             for (size_t i=0;i<szs.size();i++)
                 one_bench_pmbasis(szs[i],degs[i],field);
         }
@@ -255,6 +312,31 @@ void run_bench(long test, long nbits, bool fftprime)
             Givaro::Modular<RecInt::ruint128,RecInt::ruint256> field(zz_p::modulus());
             for (size_t i=0;i<szs.size();i++)
                 one_bench_pmbasis(szs[i],degs[i],field);
+        }
+        cout << endl << endl;
+    }
+
+    if ((test==0) | (test==3))
+    {
+        std::cout << "Bench mbasis (iterative minimal approximant basis)" << std::endl;
+        //std::cout << "rowdim\tcoldim\tdegree\tntlx\tlinbox\tntlxgen\tntl/lb\tntlxgen/ntlx" << std::endl;
+        std::cout << "rowdim\tcoldim\tdegree\tntlx\tlinbox\tntl/lb" << std::endl;
+
+        if (NumBits(zz_p::modulus()) < 26)
+        {
+            // LinBox initialize field
+            Givaro::Modular<double> field((int32_t)zz_p::modulus());
+            for (size_t i=0;i<szs.size();i++)
+                if (degs[i] <= 128)
+                    one_bench_mbasis(szs[i],degs[i],field);
+        }
+        else // nbits >= 26
+        {
+            // LinBox initialize field
+            Givaro::Modular<RecInt::ruint128,RecInt::ruint256> field(zz_p::modulus());
+            for (size_t i=0;i<szs.size();i++)
+                if (degs[i] <= 128)
+                    one_bench_mbasis(szs[i],degs[i],field);
         }
         cout << endl << endl;
     }
@@ -445,22 +527,22 @@ int main(int argc, char ** argv)
     omp_set_num_threads(1);
     //omp_set_num_threads(4);
 
-    if (argc!=3)
+    if (argc!=4)
     {
-        std::cout << "Usage: ./ntlx_linbox ./ntlx_linbox nbits test" << std::endl;
-        std::cout << "nbits : integer from 3 to 63, number of bits in prime defining the base field" << std::endl;
-        std::cout << "test = {0: all; 1: polynomial matrix multiplication; 2: pmbasis}" << std::endl;
-        //std::cout << "fftprime = {0,1} : whether to use an FFT prime, if possible" << std::endl;
+        std::cout << "Usage: ./ntlx_linbox test nbits fftprime" << std::endl;
+        std::cout << "  test = {0: all; 1: polynomial matrix multiplication; 2: pmbasis, 3: mbasis}" << std::endl;
+        std::cout << "  nbits : integer from 2 to 63, number of bits in prime defining the base field" << std::endl;
+        std::cout << "  fftprime = {0,1} : whether to use an FFT prime, if possible" << std::endl;
         return 0;
     }
 
-    long nbits = atoi(argv[1]);
-    long test = atoi(argv[2]);
-    //bool fftprime = (atoi(argv[3])==1);
+    long test = atoi(argv[1]);
+    long nbits = atoi(argv[2]);
+    bool fftprime = (atoi(argv[3])==1);
 
-    if (test < 0 || test > 2)
+    if (test < 0 || test > 3)
     {
-        std::cout << "test = {0: all; 1: polynomial matrix multiplication; 2: pmbasis}" << std::endl;
+        std::cout << "test = {0: all; 1: polynomial matrix multiplication; 2: pmbasis, 3: mbasis}" << std::endl;
         return 0;
     }
     if (nbits < 3 || nbits > 60)
@@ -469,8 +551,7 @@ int main(int argc, char ** argv)
         return 0;
     }
 
-    //run_bench(test,nbits,fftprime);
-    run_bench(test,nbits,true);
+    run_bench(test,nbits,fftprime);
 
     return 0;
 }
