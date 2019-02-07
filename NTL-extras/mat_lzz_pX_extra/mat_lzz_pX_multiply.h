d18ffddc2cbc69e37ab0a18f20a2c99a57043f47
Vincent Neiger <vincent.neiger@unilim.fr>
2019-02-07 19:40:31 +0100

    replace old FFT matmul by new ones, always faster for dimensions of interest

--- NTL-extras/mat_lzz_pX_extra/mat_lzz_pX_multiply.h
+++ NTL-extras/mat_lzz_pX_extra/mat_lzz_pX_multiply.h
@@ -227,13 +227,10 @@ void multiply_evaluate_FFT(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX
 
 /** Computes `c = a*b`. \todo short algorithm description  */
 void multiply_evaluate_FFT_matmul1(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & b);
-void multiply_evaluate_FFT_matmul1new(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & b);
 /** Computes `c = a*b`. \todo short algorithm description  */
 void multiply_evaluate_FFT_matmul2(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & b);
-void multiply_evaluate_FFT_matmul2new(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & b);
 /** Computes `c = a*b`. \todo short algorithm description  */
 void multiply_evaluate_FFT_matmul3(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & b);
-void multiply_evaluate_FFT_matmul3new(Mat<zz_pX> & c, const Mat<zz_pX> & a, const Mat<zz_pX> & b);
 
 /** Computes `c = a*b` via FFT evaluation/interpolation, but without relying on
  * `Mat<zz_p>` multiplication */
