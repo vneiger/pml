ffe4dab8b5866adf8e6b97a72e176052a97b6d76
Vincent Neiger <vneiger@users.noreply.github.com>
2022-07-24 16:26:46 +0200

    fix minor bug and simplify pivot profile

--- flint-extras/nmod_poly_mat_extra/src/nmod_poly_mat_pivot_profile.c
+++ flint-extras/nmod_poly_mat_extra/src/nmod_poly_mat_pivot_profile.c
@@ -45,58 +45,46 @@ void nmod_poly_mat_pivot_profile_columnwise(slong *pivind, slong *pivdeg, const 
 void nmod_poly_mat_pivot_profile_shifted_rowwise(slong *pivind, slong *pivdeg, const nmod_poly_mat_t mat, const slong *shift)
 {
     slong max, piv, d;
-    slong min_shift = (mat->c > 0) ? shift[0] : 0;
-
-    // find minimum of shift
-    for (slong j = 0; j < mat->c; ++j)
-        if (shift[j] < min_shift)
-            min_shift = shift[j];
 
     for (slong i = 0; i < mat->r; i++)
     {
-        max = min_shift-1; // zero rows will have this as rdeg
-        piv = -1;
+        max = -1; // zero rows will have this as pivdeg
+        piv = -1; // zero rows will have this as pivind
         for (slong j = 0; j < mat->c; j++)
         {
             d = nmod_poly_degree(nmod_poly_mat_entry(mat, i, j));
             // if new maximum (or equal maximum) reached at a nonzero entry, update
-            if (0 <= d && max <= d+shift[j])
+            if (0 <= d && (piv == -1 || max <= d+shift[j]))
             {
-                max = d;
                 piv = j;
+                max = d+shift[piv];
             }
         }
-        pivdeg[i] = max;
         pivind[i] = piv;
+        pivdeg[i] = (piv == -1) ? -1 : (max - shift[piv]);
     }
 }
 
 void nmod_poly_mat_pivot_profile_shifted_columnwise(slong *pivind, slong *pivdeg, const nmod_poly_mat_t mat, const slong *shift)
 {
     slong max, piv, d;
-    slong min_shift = (mat->r > 0) ? shift[0] : 0;
-
-    // find minimum of shift
-    for (slong i = 0; i < mat->r; ++i)
-        if (shift[i] < min_shift)
-            min_shift = shift[i];
 
     for (slong j = 0; j < mat->c; j++)
     {
-        max = min_shift-1;
-        piv = -1;
+        max = -1; // zero columns will have this as pivdeg
+        piv = -1; // zero columns will have this as pivind
         for (slong i = 0; i < mat->r; i++)
         {
             d = nmod_poly_degree(nmod_poly_mat_entry(mat, i, j));
             // if new maximum (or equal maximum) reached at a nonzero entry, update
-            if (0 <= d && max <= d+shift[i])
+            if (0 <= d && (piv == -1 || max <= d+shift[i]))
             {
-                max = d;
                 piv = i;
+                max = d+shift[piv];
             }
         }
-        pivdeg[j] = max;
         pivind[j] = piv;
+        pivdeg[j] = (piv == -1) ? -1 : (max - shift[piv]);
     }
 }
 
