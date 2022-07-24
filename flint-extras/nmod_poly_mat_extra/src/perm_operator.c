cb17f1bbc229719f7dffbc54154133ea17eb0800
Vincent Neiger <vneiger@users.noreply.github.com>
2022-07-25 00:33:48 +0200

    wip

--- flint-extras/nmod_poly_mat_extra/src/perm_operator.c
+++
@@ -1,9 +0,0 @@
-#include "nmod_poly_mat_approximant.h"
-
-/**
- * \brief A function fund in the doc of nmod_mat_t
- *
- */
-
-/* -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-// vim:sts=4:sw=4:ts=4:et:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
