e3487e68f4a22951dcee03628d0da565ca1fb9c5
Vincent Neiger <vneiger@users.noreply.github.com>
2023-10-16 22:08:41 +0200

    starting weak Popov

---
+++ flint-extras/nmod_poly_mat_extra/src/nmod_poly_mat_weak_popov.c
@@ -0,0 +1,14 @@
+#include <flint/flint.h>
+#include <flint/nmod_poly.h>
+#include <flint/nmod_vec.h>
+#include "nmod_poly_mat_utils.h"
+#include "nmod_poly_mat_forms.h"
+#include "nmod_poly_mat_io.h" // TODO remove, for debugging
+
+void _nmod_poly_mat_handle_pivot_collision(nmod_poly_mat_t mat, nmod_poly_mat_t tsf,
+                                           slong i, slong ii, slong j)
+{
+}
+
+/* -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+// vim:sts=4:sw=4:ts=4:et:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
