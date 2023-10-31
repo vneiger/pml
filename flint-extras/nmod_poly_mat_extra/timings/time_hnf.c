dde0e91d82a3ec9ad6753921d02e754ce1fc61ed
Vincent Neiger <vneiger@users.noreply.github.com>
2023-10-31 13:03:40 +0100

    easier access to variants for parallel benchmarking

--- flint-extras/nmod_poly_mat_extra/timings/time_hnf.c
+++ flint-extras/nmod_poly_mat_extra/timings/time_hnf.c
@@ -15,7 +15,7 @@ static void warmup(flint_rand_t state)
     nmod_mat_clear(mat);
 }
 
-// main function
+// main benchmark, with transformation
 void benchmark_generic_hnf_withtrans(slong rdim, slong cdim, slong len,
                                      ulong prime, flint_rand_t state)
 {
@@ -161,6 +161,142 @@ void benchmark_generic_hnf_withtrans(slong rdim, slong cdim, slong len,
     flint_free(pivind);
 }
 
+// main benchmark, without transformation
+void benchmark_generic_hnf_notrans(slong rdim, slong cdim, slong len,
+                                   ulong prime, flint_rand_t state)
+{
+    // create random matrix
+    nmod_poly_mat_t mat;
+    nmod_poly_mat_init(mat, rdim, cdim, prime);
+    nmod_poly_mat_rand(mat, state, len);
+
+    // init copy of mat
+    nmod_poly_mat_t hnf;
+    nmod_poly_mat_init(hnf, mat->r, mat->c, mat->modulus);
+
+    // initialize pivind list and rank profiles
+    slong max_rank = FLINT_MIN(mat->r, mat->c);
+    slong * pivind = flint_malloc(max_rank * sizeof(slong));
+
+    // for timing
+    long thres = 1000; // 1000ms = 1s
+    long nb_iter = 0;
+    long t_uref = 0; // time for uref, in ms
+    long t_norm = 0; // time for normalization, in ms
+    timeit_t timer;
+
+    flint_printf("%ld\t%ld\t%ld\t", rdim, cdim, len);
+
+    { // maxdeg, atomic
+        nb_iter = 0; t_uref = 0; t_norm = 0;
+        while (t_uref+t_norm < thres)
+        {
+            nmod_poly_mat_set(hnf, mat);
+            timeit_start(timer);
+            slong rk = nmod_poly_mat_uref_maxdeg_atomic(hnf, NULL, pivind);
+            timeit_stop(timer);
+            t_uref += timer->wall;
+            timeit_start(timer);
+            _normalize_uref(hnf, NULL, pivind, rk);
+            timeit_stop(timer);
+            t_norm += timer->wall;
+            nb_iter++;
+        }
+        flint_printf("%0.1e\t%0.1e\t%0.1e\t",
+                     (double)t_uref/nb_iter,
+                     (double)t_norm/nb_iter,
+                     (double)(t_uref+t_norm) / nb_iter);
+    }
+
+    { // revlex, xgcd
+        nb_iter = 0; t_uref = 0; t_norm = 0;
+        while (t_uref+t_norm < thres)
+        {
+            nmod_poly_mat_set(hnf, mat);
+            timeit_start(timer);
+            slong rk = nmod_poly_mat_uref_revlex_xgcd(hnf, NULL, pivind, NULL);
+            timeit_stop(timer);
+            t_uref += timer->wall;
+            timeit_start(timer);
+            _normalize_uref(hnf, NULL, pivind, rk);
+            timeit_stop(timer);
+            t_norm += timer->wall;
+            nb_iter++;
+        }
+        flint_printf("%0.1e\t%0.1e\t%0.1e\t",
+                     (double)t_uref/nb_iter,
+                     (double)t_norm/nb_iter,
+                     (double)(t_uref+t_norm) / nb_iter);
+    }
+
+    { // lex, xgcd
+        nb_iter = 0; t_uref = 0; t_norm = 0;
+        while (t_uref+t_norm < thres)
+        {
+            nmod_poly_mat_set(hnf, mat);
+            timeit_start(timer);
+            slong rk = nmod_poly_mat_uref_lex_xgcd(hnf, NULL, pivind, NULL);
+            timeit_stop(timer);
+            t_uref += timer->wall;
+            timeit_start(timer);
+            _normalize_uref(hnf, NULL, pivind, rk);
+            timeit_stop(timer);
+            t_norm += timer->wall;
+            nb_iter++;
+        }
+        flint_printf("%0.1e\t%0.1e\t%0.1e\t",
+                     (double)t_uref/nb_iter,
+                     (double)t_norm/nb_iter,
+                     (double)(t_uref+t_norm) / nb_iter);
+    }
+
+    { // Kannan-Bachem's algorithm
+        nb_iter = 0; t_uref = 0;
+        while (t_uref < thres)
+        {
+            nmod_poly_mat_set(hnf, mat);
+            timeit_start(timer);
+            nmod_poly_mat_hnf_ur_revlex_xgcd_delayed_zero(hnf, NULL, pivind, NULL);
+            timeit_stop(timer);
+            t_uref += timer->wall;
+            nb_iter++;
+        }
+        flint_printf("%0.1e\t", (double)t_uref/nb_iter);
+    }
+
+    { // Mulder-Storjohann's algorithm
+        nb_iter = 0; t_uref = 0; t_norm = 0;
+        slong rk = 0;
+        while (t_uref+t_norm < thres)
+        {
+            nmod_poly_mat_set(hnf, mat);
+            timeit_start(timer);
+            rk = nmod_poly_mat_uref_matrixgcd_iter(hnf, NULL, pivind, NULL, NULL);
+            timeit_stop(timer);
+            t_uref += timer->wall;
+            if (rk > 0 || nmod_poly_mat_is_zero(mat)) // avoid non-generic early exit
+            {
+                timeit_start(timer);
+                _normalize_uref(hnf, NULL, pivind, rk);
+                timeit_stop(timer);
+                t_norm += timer->wall;
+            }
+            nb_iter++;
+        }
+        if (rk <= 0 && !nmod_poly_mat_is_zero(mat)) // early exit, non-generic
+            flint_printf("%0.1e\tXXX\tXXX\t", (double)t_uref/nb_iter);
+        else
+            flint_printf("%0.1e\t%0.1e\t%0.1e\t",
+                        (double)t_uref/nb_iter,
+                        (double)t_norm/nb_iter,
+                        (double)(t_uref+t_norm) / nb_iter);
+    }
+
+    flint_printf("\n");
+    nmod_poly_mat_clear(hnf);
+    flint_free(pivind);
+}
+
 /** Launches a series of benchmarks for a given prime size.
  *
  *  Launches benchmark for many matrix dimensions and lengths for a given
@@ -170,7 +306,7 @@ void benchmark_generic_hnf_withtrans(slong rdim, slong cdim, slong len,
  * \param state flint's random generator
  * \return void
  */
-void benchmark_nbits(ulong nbits, flint_rand_t state)
+void benchmark_nbits(ulong nbits, int tsf, int shape, flint_rand_t state)
 {
     flint_randinit(state);
     const ulong prime = n_randprime(state, nbits, 0);
@@ -179,7 +315,10 @@ void benchmark_nbits(ulong nbits, flint_rand_t state)
     slong lens[] = { 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192};
 
     printf("------------------------\n");
-    printf("Bench HNF (times in ms)\n");
+    if (tsf)
+        printf("Bench HNF, with transformation (times in ms):\n");
+    else
+        printf("Bench HNF, without transformation (times in ms):\n");
     printf("nbits=%ld, prime=%ld\n", nbits, prime);
     if (nbits < 15)
         printf("Warning: for 'small' primes, risk of non-generic CRP; Mulders-Storjohann's HNF may early exit before the computation is complete\n");
@@ -201,11 +340,26 @@ void benchmark_nbits(ulong nbits, flint_rand_t state)
         for (size_t j = 0; j < sizeof(lens) / sizeof(lens[0]); ++j)
         {
             long len = lens[j];
-            benchmark_generic_hnf_withtrans(rdim, 1, len, prime, state);
-            benchmark_generic_hnf_withtrans(1, rdim, len, prime, state);
-            benchmark_generic_hnf_withtrans(rdim, rdim/2, len, prime, state);
-            benchmark_generic_hnf_withtrans(rdim/2, rdim, len, prime, state);
-            benchmark_generic_hnf_withtrans(rdim, rdim, len, prime, state);
+            if (tsf) // with transformation
+                switch (shape)
+                {
+                case 0: benchmark_generic_hnf_withtrans(rdim, rdim, len, prime, state); break;
+                case 1: benchmark_generic_hnf_withtrans(rdim/2, rdim, len, prime, state); break;
+                case 2: benchmark_generic_hnf_withtrans(rdim, rdim/2, len, prime, state); break;
+                case 3: benchmark_generic_hnf_withtrans(1, rdim, len, prime, state); break;
+                case 4: benchmark_generic_hnf_withtrans(rdim, 1, len, prime, state); break;
+                default: break;
+                }
+            else // no transformation
+                switch (shape)
+                {
+                case 0: benchmark_generic_hnf_notrans(rdim, rdim, len, prime, state); break;
+                case 1: benchmark_generic_hnf_notrans(rdim/2, rdim, len, prime, state); break;
+                case 2: benchmark_generic_hnf_notrans(rdim, rdim/2, len, prime, state); break;
+                case 3: benchmark_generic_hnf_notrans(1, rdim, len, prime, state); break;
+                case 4: benchmark_generic_hnf_notrans(rdim, 1, len, prime, state); break;
+                default: break;
+                }
         }
     }
 
@@ -223,11 +377,14 @@ void benchmark_nbits(ulong nbits, flint_rand_t state)
  * \param state flint's random generator
  * \return void
  */
-void benchmark_nbits_dim_deg(ulong nbits, ulong rdim, ulong cdim, ulong len, flint_rand_t state)
+void benchmark_nbits_dim_deg(ulong nbits, ulong rdim, ulong cdim, ulong len, int tsf, flint_rand_t state)
 {
     const ulong prime = n_randprime(state, nbits, 0);
 
-    printf("Bench HNF (times in ms):\n");
+    if (tsf)
+        printf("Bench HNF, with transformation (times in ms):\n");
+    else
+        printf("Bench HNF, without transformation (times in ms):\n");
     printf("nbits=%ld, prime=%ld, rdim=%ld, cdim=%ld, len=%ld\n",nbits, prime, rdim, cdim, len);
     if (nbits < 15)
         printf("Warning: for 'small' primes, risk of non-generic CRP; Mulders-Storjohann's HNF may early exit before the computation is complete\n");
@@ -241,7 +398,10 @@ void benchmark_nbits_dim_deg(ulong nbits, ulong rdim, ulong cdim, ulong len, fli
     printf("KB-tot\t");
     printf("mx-ef\tmx-nm\tmx-tot\t");
     printf("\n");
-    benchmark_generic_hnf_withtrans(rdim, cdim, len, prime, state);
+    if (tsf)
+        benchmark_generic_hnf_withtrans(rdim, cdim, len, prime, state);
+    else
+        benchmark_generic_hnf_notrans(rdim, cdim, len, prime, state);
 }
 
 int main(int argc, char *argv[])
@@ -252,28 +412,40 @@ int main(int argc, char *argv[])
     flint_randinit(state);
     flint_randseed(state, rand(), rand());
 
-    if (argc!=2 && argc!=5)
+    if (argc!=4 && argc!=6)
     {
-        printf("Usage: %s nbits OR %s nbits rdim cdim len\n",argv[0],argv[0]);
+        printf("Usage: %s nbits tsf shape OR %s nbits rdim cdim len tsf\n",argv[0],argv[0]);
+        printf("  tsf:\n");
+        printf("      0  --> without transformation\n");
+        printf("      1  --> with transformation\n");
+        printf("  shape:\n");
+        printf("      0  --> square n x n\n");
+        printf("      1  --> rectangular n/2 x n\n");
+        printf("      2  --> rectangular n x n/2\n");
+        printf("      3  --> vector 1 x n\n");
+        printf("      4  --> vector n x 1\n");
         return 1;
     }
 
-    if (argc==2)
+    if (argc==4)
     {
         warmup(state);
         ulong nbits = atoi(argv[1]);
-        benchmark_nbits(nbits, state);
+        int tsf = atoi(argv[2]);
+        int shape = atoi(argv[3]);
+        benchmark_nbits(nbits, tsf, shape, state);
         return 0;
     }
 
-    if (argc==5)
+    if (argc==6)
     {
         warmup(state);
         ulong nbits = atoi(argv[1]);
         ulong rdim = atoi(argv[2]);
         ulong cdim = atoi(argv[3]);
         ulong deg = atoi(argv[4]);
-        benchmark_nbits_dim_deg(nbits,rdim,cdim,deg,state);
+        int tsf = atoi(argv[5]);
+        benchmark_nbits_dim_deg(nbits,rdim,cdim,deg,tsf,state);
     }
 
     flint_randclear(state);
