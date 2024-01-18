ffdffc4a79e77856a1d4d3b30c7515555f513ac4
schost <eric.schost@gmail.com>
2024-01-18 09:28:40 -0500

    middle product for polynomial matrices

---
+++ flint-extras/nmod_poly_mat_extra/src/nmod_poly_mat_middle_product_naive.c
@@ -0,0 +1,16 @@
+#include <flint/nmod_mat.h>
+
+#include "nmod_poly_mat_multiply.h"
+
+/** Middle product for polynomial matrices
+ *  sets C = ((A * B) div x^dA) mod x^(dB+1), assuming deg(A) <= dA and deg(B) <= dA + dB
+ *  output can alias input
+ *  naive implementation (multiply, shift, truncate)
+ */
+void nmod_poly_mat_middle_product_naive(nmod_poly_mat_t C, const nmod_poly_mat_t A, const nmod_poly_mat_t B,
+                                        const ulong dA, const ulong dB)
+{
+    nmod_poly_mat_mul(C, A, B);
+    nmod_poly_mat_shift_right(C, C, dA);
+    nmod_poly_mat_truncate(C, dB + 1);
+}
