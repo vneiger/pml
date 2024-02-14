3130d2a615833d5e8809f241c2caa48dab70080d
Vincent Neiger <vneiger@users.noreply.github.com>
2024-02-14 14:51:04 +0100

    remove old version of pmbasis

--- flint-extras/nmod_poly_mat_extra/src/nmod_poly_mat_approximant_pmbasis.c
+++ flint-extras/nmod_poly_mat_extra/src/nmod_poly_mat_approximant_pmbasis.c
@@ -14,39 +14,6 @@ void middle_product(nmod_poly_mat_t res,
 }
 
 
-void pmbasis(nmod_poly_mat_t res, slong *res_shifts,
-              const nmod_poly_mat_t F, ulong sigma, const slong *shifts)
-{
-    slong rdim = F->r, cdim = F->c;
-    mp_limb_t prime = F->modulus;
-    nmod_poly_mat_t Pl, Ph, F_prime;
-    slong ul[rdim];
-
-    if (sigma <= PMBASIS_THRES)
-    {
-        mbasisIII(res, res_shifts, F, sigma, shifts);
-        return;
-    }
-
-    nmod_poly_mat_init(Pl, rdim, rdim, prime);
-    pmbasis(Pl, ul, F, sigma / 2, shifts);
-
-    nmod_poly_mat_init(F_prime, rdim, cdim, prime);
-    middle_product(F_prime, Pl, F, sigma / 2, sigma);
-
-    nmod_poly_mat_init(Ph, rdim, rdim, prime);
-    if (sigma % 2 == 0)
-        pmbasis(Ph, res_shifts, F_prime, sigma / 2, ul);
-    else
-        pmbasis(Ph, res_shifts, F_prime, sigma / 2 + 1, ul);
-
-    nmod_poly_mat_mul(res, Ph, Pl);
-
-    nmod_poly_mat_clear(Ph);
-    nmod_poly_mat_clear(Pl);
-    nmod_poly_mat_clear(F_prime);
-}
-
 void nmod_poly_mat_pmbasis(nmod_poly_mat_t appbas,
                            slong * shift,
                            const nmod_poly_mat_t pmat,
