8db673bf3181f1ddc11c6d9de5ff88d82097c0c8
Vincent Neiger <vneiger@users.noreply.github.com>
2024-02-19 09:41:15 +0100

    "modeline

--- flint-extras/nmod_vec_extra/src/nmod_vec_integer_dot_product.c
+++ flint-extras/nmod_vec_extra/src/nmod_vec_integer_dot_product.c
@@ -80,3 +80,6 @@ void nmod_vec_integer_dot_product(mp_ptr res, mp_srcptr v1, mp_srcptr v2, ulong 
 
     return;
 }
+
+/* -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+// vim:sts=4:sw=4:ts=4:et:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
