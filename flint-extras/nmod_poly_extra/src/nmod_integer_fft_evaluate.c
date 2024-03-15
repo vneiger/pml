fa4c3c20787f3f13002d8f847adcd3515b511778
Vincent Neiger <vneiger@users.noreply.github.com>
2024-03-15 20:11:14 +0100

    wip

--- flint-extras/nmod_poly_extra/src/nmod_integer_fft_evaluate.c
+++ flint-extras/nmod_poly_extra/src/nmod_integer_fft_evaluate.c
@@ -251,45 +251,47 @@ void _nmod_poly_dif_inplace_radix2_rec_v4(mp_ptr p, ulong len, ulong order, nmod
         // p[1] = (p[0] + p[2]) - (p[1] + p[3]);
         // p[2] = (p[0] - p[2]) + I*(p[1] - p[3]);
         // p[3] = (p[0] - p[2]) - I*(p[1] - p[3]);
-        mp_limb_t tmp02 = nmod_add(p[0], p[2], F->mod); // p[0] + p[2], [0,n-1]
-        p[2] = nmod_sub(p[0], p[2], F->mod);    // p[0] - p[2], [0,n-1]
-        mp_limb_t tmp13 = nmod_sub(p[1], p[3], F->mod); // p[1] - p[3], [0,n-1]
-        p[1] = nmod_add(p[1], p[3], F->mod);  // p[1] + p[3], [0,2n-2]
-
-        p[0] = nmod_add(tmp02, p[1], F->mod);  // p[0] now ok,  [0, n-1]
-        p[1] = nmod_sub(tmp02, p[1], F->mod);  // p[1] now ok,  [0, n-1]
-
-        // compute I*(p[1] - p[3]),  [0, n-1]
-        tmp13 = n_mulmod_shoup(F->tab_w[0][1], tmp13, F->tab_w_pre[0][1], F->mod.n);
-        p[3] = nmod_sub(p[2], tmp13, F->mod); // p[3] now ok, [0, n-1]
-        p[2] = nmod_add(p[2], tmp13, F->mod); // p[2] now ok, [0, n-1]
+        const mp_limb_t p0 = p[0];
+        const mp_limb_t p1 = p[1];
+        const mp_limb_t p2 = p[2];
+        const mp_limb_t p3 = p[3];
+        const mp_limb_t p4 = nmod_add(p0, p2, F->mod);
+        const mp_limb_t p5 = nmod_sub(p0, p2, F->mod);
+        const mp_limb_t p6 = nmod_add(p1, p3, F->mod);
+        const mp_limb_t p7 = n_mulmod_shoup(F->tab_w[0][1], nmod_sub(p1, p3, F->mod), F->tab_w_pre[0][1], F->mod.n);
+        p[0] = nmod_add(p4, p6, F->mod);
+        p[1] = nmod_sub(p4, p6, F->mod);
+        p[2] = nmod_add(p5, p7, F->mod);
+        p[3] = nmod_sub(p5, p7, F->mod);
     }
     else
     {
         // here order >= 3, len >= 8, try some unrolling
-        const mp_ptr phi = p+(len/2);
-        mp_limb_t tmp[4];
+        const mp_ptr p0 = p;
+        const mp_ptr p1 = p+(len/2);
+        const mp_ptr ww = F->tab_w[order-2];
+        const mp_ptr wwpre = F->tab_w_pre[order-2];
         for (ulong k = 0; k < len/2; k+=4)
         {
-            tmp[0] = p[k+0];
-            tmp[1] = p[k+1];
-            tmp[2] = p[k+2];
-            tmp[3] = p[k+3];
-            p[k+0] = nmod_add(p[k+0], phi[k+0], F->mod);
-            p[k+1] = nmod_add(p[k+1], phi[k+1], F->mod);
-            p[k+2] = nmod_add(p[k+2], phi[k+2], F->mod);
-            p[k+3] = nmod_add(p[k+3], phi[k+3], F->mod);
-            phi[k+0] = nmod_sub(tmp[0], phi[k+0], F->mod);
-            phi[k+1] = nmod_sub(tmp[1], phi[k+1], F->mod);
-            phi[k+2] = nmod_sub(tmp[2], phi[k+2], F->mod);
-            phi[k+3] = nmod_sub(tmp[3], phi[k+3], F->mod);
-            phi[k+0] = n_mulmod_shoup(F->tab_w[order-2][k+0], phi[k+0], F->tab_w_pre[order-2][k+0], F->mod.n);
-            phi[k+1] = n_mulmod_shoup(F->tab_w[order-2][k+1], phi[k+1], F->tab_w_pre[order-2][k+1], F->mod.n);
-            phi[k+2] = n_mulmod_shoup(F->tab_w[order-2][k+2], phi[k+2], F->tab_w_pre[order-2][k+2], F->mod.n);
-            phi[k+3] = n_mulmod_shoup(F->tab_w[order-2][k+3], phi[k+3], F->tab_w_pre[order-2][k+3], F->mod.n);
+            const mp_limb_t u0 = p0[k+0];
+            const mp_limb_t u1 = p0[k+1];
+            const mp_limb_t u2 = p0[k+2];
+            const mp_limb_t u3 = p0[k+3];
+            const mp_limb_t v0 = p1[k+0];
+            const mp_limb_t v1 = p1[k+1];
+            const mp_limb_t v2 = p1[k+2];
+            const mp_limb_t v3 = p1[k+3];
+            p0[k+0] = nmod_add(u0, v0, F->mod);
+            p0[k+1] = nmod_add(u1, v1, F->mod);
+            p0[k+2] = nmod_add(u2, v2, F->mod);
+            p0[k+3] = nmod_add(u3, v3, F->mod);
+            p1[k+0] = n_mulmod_shoup(ww[k+0], nmod_sub(u0, v0, F->mod), wwpre[k+0], F->mod.n);
+            p1[k+1] = n_mulmod_shoup(ww[k+1], nmod_sub(u1, v1, F->mod), wwpre[k+1], F->mod.n);
+            p1[k+2] = n_mulmod_shoup(ww[k+2], nmod_sub(u2, v2, F->mod), wwpre[k+2], F->mod.n);
+            p1[k+3] = n_mulmod_shoup(ww[k+3], nmod_sub(u3, v3, F->mod), wwpre[k+3], F->mod.n);
         }
-        _nmod_poly_dif_inplace_radix2_rec_v4(p, len/2, order-1, F);
-        _nmod_poly_dif_inplace_radix2_rec_v4(phi, len/2, order-1, F);
+        _nmod_poly_dif_inplace_radix2_rec_v4(p0, len/2, order-1, F);
+        _nmod_poly_dif_inplace_radix2_rec_v4(p1, len/2, order-1, F);
     }
 
 }
