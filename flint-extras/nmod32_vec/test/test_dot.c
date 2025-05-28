adbadd9afa14f97a883d594bdb47b69771d481a6
Vincent Neiger <vneiger@users.noreply.github.com>
2025-05-28 21:17:09 +0200

    test file

---
+++ flint-extras/nmod32_vec/test/test_dot.c
@@ -0,0 +1,76 @@
+#include "flint/test_helpers.h"
+#include "flint/nmod.h"
+#include "flint/nmod_vec.h"
+
+#include "nmod32_vec.h"
+
+// utility (nmod vec uniform random)
+static inline
+void _nmod32_vec_rand(n32_ptr vec, flint_rand_t state, slong len, nmod_t mod)
+{
+    for (slong i = 0; i < len; i++)
+        vec[i] = n_randint(state, mod.n);
+}
+
+TEST_FUNCTION_START(nmod_vec_dot, state)
+{
+    int i;
+
+    for (i = 0; i < 1000 * flint_test_multiplier(); i++)
+    {
+        slong len;
+        nmod_t mod;
+        ulong m, res;
+        n32_ptr x, y;
+        nn_ptr xx, yy;
+        ulong correct;
+
+        len = n_randint(state, 1000) + 1;
+        m = n_randtest_not_zero(state) % (UWORD(1) << 32);
+
+        nmod_init(&mod, m);
+
+        const dot_params_t params = _nmod_vec_dot_params(len, mod);
+
+        x = _nmod32_vec_init(len);
+        y = _nmod32_vec_init(len);
+        xx = _nmod_vec_init(len);
+        yy = _nmod_vec_init(len);
+
+        _nmod32_vec_rand(x, state, len, mod);
+        _nmod32_vec_rand(y, state, len, mod);
+        for (slong k = 0; k < len; k++)
+        {
+            xx[k] = x[k];
+            yy[k] = y[k];
+        }
+        correct = _nmod_vec_dot(xx, yy, len, mod, params);
+
+        {  // dot2_half_avx2
+            res = _nmod32_vec_dot2_split_avx2(x, y, len, mod, params.pow2_precomp);
+
+            if ((ulong)res != correct)
+                TEST_FUNCTION_FAIL("m = %wu\n"
+                                   "len = %wd\n",
+                                   m, len);
+
+        }
+
+        _nmod32_vec_clear(x);
+        _nmod32_vec_clear(y);
+        _nmod_vec_clear(xx);
+        _nmod_vec_clear(yy);
+    }
+
+    TEST_FUNCTION_END(state);
+}
+
+int main()
+{
+    flint_rand_t state;
+    flint_rand_init(state);
+    flint_rand_set_seed(state, time(NULL), time(NULL)+12984125L);
+
+    test_nmod_vec_dot();
+    return 0;
+}
