c0480d627be74bf52a52c7a05becb809a8f7c1b3
Vincent Neiger <vneiger@users.noreply.github.com>
2025-05-29 08:35:47 +0200

    test

--- flint-extras/nmod32_vec/test/test_dot.c
+++ flint-extras/nmod32_vec/test/test_dot.c
@@ -15,24 +15,31 @@ void _nmod32_vec_rand(n32_ptr vec, flint_rand_t state, slong len, nmod_t mod)
 
 TEST_FUNCTION_START(nmod_vec_dot, state)
 {
+    flint_rand_set_seed(state, time(NULL), time(NULL)+12984125L);
+
     int i;
 
-    for (i = 0; i < 1000 * flint_test_multiplier(); i++)
+    for (i = 0; i < 5000 * flint_test_multiplier(); i++)
     {
-        slong len;
-        nmod_t mod;
+        const int acc8 = (i < 2500);
+
+        slong len = n_randint(state, 1000) + 1;
+
+        const long pow2_61_sqrt = UWORD(1518500249);  // floor(2**30.5)
+        const long pow2_31 = UWORD(1) << 31;
         ulong m = 0;
-        ulong res;
-        n32_ptr x, y;
-        nn_ptr xx, yy;
-        ulong correct;
+        if (acc8)
+        {
+            while (m == 0)
+                m = n_randtest_not_zero(state) % pow2_61_sqrt;
+        }
+        else
+        {
+            while (m == 0)
+                m = n_randtest_not_zero(state) % pow2_31;
+        }
 
-        len = n_randint(state, 1000) + 1;
-        const long sqrt61 = UWORD(1518500249);  // floor(2**30.5)
-        while (m == 0)
-            //m = n_randtest_not_zero(state) % (UWORD(1) << 30);
-            m = n_randtest_not_zero(state) % sqrt61;
-
+        nmod_t mod;
         nmod_init(&mod, m);
 
         const dot_params_t params = _nmod_vec_dot_params(len, mod);
@@ -40,10 +47,10 @@ TEST_FUNCTION_START(nmod_vec_dot, state)
         ulong pow2_precomp;
         NMOD_RED(pow2_precomp, (UWORD(1) << DOT_SPLIT_BITS), mod);
 
-        x = _nmod32_vec_init(len);
-        y = _nmod32_vec_init(len);
-        xx = _nmod_vec_init(len);
-        yy = _nmod_vec_init(len);
+        n32_ptr x = _nmod32_vec_init(len);
+        n32_ptr y = _nmod32_vec_init(len);
+        nn_ptr xx = _nmod_vec_init(len);
+        nn_ptr yy = _nmod_vec_init(len);
 
         _nmod32_vec_rand(x, state, len, mod);
         _nmod32_vec_rand(y, state, len, mod);
@@ -52,12 +59,12 @@ TEST_FUNCTION_START(nmod_vec_dot, state)
             xx[k] = x[k];
             yy[k] = y[k];
         }
-        correct = _nmod_vec_dot(xx, yy, len, mod, params);
+        ulong correct = _nmod_vec_dot(xx, yy, len, mod, params);
 
         {  // dot_split
-            res = _nmod32_vec_dot_split(x, y, len, mod, pow2_precomp);
+            ulong res = _nmod32_vec_dot_split(x, y, len, mod, pow2_precomp);
 
-            if ((ulong)res != correct)
+            if (res != correct)
             {
                 flint_printf("%ld\n", i);
                 TEST_FUNCTION_FAIL("dot_split, m = %wu, len = %wd\n", m, len);
@@ -65,9 +72,9 @@ TEST_FUNCTION_START(nmod_vec_dot, state)
         }
 
         {  // dot_split_avx2
-            res = _nmod32_vec_dot_split_avx2(x, y, len, mod, pow2_precomp);
+            ulong res = _nmod32_vec_dot_split_avx2(x, y, len, mod, pow2_precomp);
 
-            if ((ulong)res != correct)
+            if (res != correct)
             {
                 flint_printf("%ld\n", i);
                 TEST_FUNCTION_FAIL("dot_split_avx2, m = %wu, len = %wd\n", m, len);
@@ -75,9 +82,9 @@ TEST_FUNCTION_START(nmod_vec_dot, state)
         }
 
         {  // dot_split_avx512
-            res = _nmod32_vec_dot_split_avx512(x, y, len, mod, pow2_precomp);
+            ulong res = _nmod32_vec_dot_split_avx512(x, y, len, mod, pow2_precomp);
 
-            if ((ulong)res != correct)
+            if (res != correct)
             {
                 flint_printf("%ld\n", i);
                 TEST_FUNCTION_FAIL("dot_split_avx512, m = %wu, len = %wd\n", m, len);
@@ -85,9 +92,9 @@ TEST_FUNCTION_START(nmod_vec_dot, state)
         }
 
         {  // dot_msolve_avx2
-            res = _nmod32_vec_dot_msolve_avx2(x, y, len, mod.n);
+            ulong res = _nmod32_vec_dot_msolve_avx2(x, y, len, mod.n);
 
-            if ((ulong)res != correct)
+            if (res != correct)
             {
                 flint_printf("%ld\n", i);
                 TEST_FUNCTION_FAIL("dot_msolve_avx2, m = %wu, len = %wd\n", m, len);
@@ -105,10 +112,6 @@ TEST_FUNCTION_START(nmod_vec_dot, state)
 
 int main()
 {
-    flint_rand_t state;
-    flint_rand_init(state);
-    flint_rand_set_seed(state, time(NULL), time(NULL)+12984125L);
-
     test_nmod_vec_dot();
     return 0;
 }
