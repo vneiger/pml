b66189be79cf93e1827c1e4ed372fece47081e3d
Gilles Villard <gilles.villard@ens-lyon.fr>
2025-06-11 15:23:12 +0200

    Updated copyrights with PML

--- flint-extras/src/mapml/mapml_matpoly_export.h
+++ flint-extras/src/mapml/mapml_matpoly_export.h
@@ -1,20 +1,14 @@
 /* 
    Copyright (C) 2024 Gilles Villard
 
-   This file is part of mapml. mapml is free software: you
-   can redistribute it and/or modify it under the terms of the GNU Lesser
-   General Public License as published by the Free Software Foundation,
-   either version 2.1 of the License, or (at your option) any later version.
+    This file is part of PML.
 
-   mapml is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   GNU Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with mapml. If not, see <http://www.gnu.org/licenses/>. */
-
-// *******************************************************
+    PML is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License version 2.0 (GPL-2.0-or-later)
+    as published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version. See
+    <https://www.gnu.org/licenses/>.
+*/
 
 
 #ifndef MAPML_MATPOLY_EXPORT_H
