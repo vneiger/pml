0be1167234fdfd60da2fbbd5c0d73dfa200dc6b5
Vincent Neiger <vneiger@users.noreply.github.com>
2025-06-12 19:42:59 +0200

    t-det ok

--- flint-extras/src/nmod_poly_mat_extra/test/main.c
+++ flint-extras/src/nmod_poly_mat_extra/test/main.c
@@ -1,18 +1,26 @@
 /*
-    This file is part of PML
+    Copyright (C) 2025 Vincent Neiger, Éric Schost, Gilles Villard
 
-    Using FLINT functionalities 
+    This file is part of PML.
+
+    PML is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License version 2.0 (GPL-2.0-or-later)
+    as published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version. See
+    <https://www.gnu.org/licenses/>.
 */
 
 /* Include functions *********************************************************/
 
+#include "t-det.c"
 #include "t-dixon.c"
 
 /* Array of test functions ***************************************************/
 
 test_struct tests[] =
 {
-    TEST_FUNCTION(nmod_poly_mat_dixon)
+    TEST_FUNCTION(nmod_poly_mat_dixon),
+    TEST_FUNCTION(nmod_poly_mat_det)
 };
 
 /* main function *************************************************************/
