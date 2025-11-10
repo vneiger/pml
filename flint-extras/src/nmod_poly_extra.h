2c2b8b5e75eb2f801dda8053e22647158e54f97d
Vincent Neiger <vneiger@users.noreply.github.com>
2025-11-10 20:16:22 +0100

    remove geometric progression MPE/interp, now in FLINT

--- flint-extras/src/nmod_poly_extra.h
+++ flint-extras/src/nmod_poly_extra.h
@@ -22,7 +22,6 @@
 extern "C" {
 #endif
 
-
 /** Generates random polynomial `pol` of length up to `len` with uniformly
  * random coefficients. If `len` is nonpositive, `pol` is set to zero. */
 void nmod_poly_rand(nmod_poly_t pol,
@@ -36,49 +35,6 @@ void nmod_poly_rand(nmod_poly_t pol,
 void nmod_poly_rand_monic(nmod_poly_t pol,
                           flint_rand_t state,
                           slong len);
-
-
-
-/*------------------------------------------------------------*/
-/* a structure for geometric evaluation / interpolation       */
-/* TODO: handle non-existence                                 */
-/*------------------------------------------------------------*/
-typedef struct
-{
-    nn_ptr x, t, w, y, z;       // five vectors of precomputed constants
-    nmod_poly_t f, g1, g2;      // three precomputed polys
-    nmod_t mod;
-    slong d;                    // number of points
-
-} nmod_geometric_progression_struct;
-
-typedef nmod_geometric_progression_struct nmod_geometric_progression_t[1];
-
-/*------------------------------------------------------------*/
-/* initializes all quantities attached to G                   */
-/* evaluates/interpolates at powers of q = r^2                */
-/*------------------------------------------------------------*/
-void nmod_geometric_progression_init_set(nmod_geometric_progression_t G, ulong r, slong n, nmod_t mod);
- 
-/*------------------------------------------------------------*/
-/* frees all memory attached to G                             */
-/*------------------------------------------------------------*/
-void nmod_geometric_progression_clear(nmod_geometric_progression_t G);
-
-/*------------------------------------------------------------*/
-/* in: polynomial to evaluate, deg(poly) < d                  */
-/* out: v[i] = poly(q^i), i = 0 .. d-1                        */
-/*------------------------------------------------------------*/
-void nmod_geometric_progression_evaluate(nn_ptr v, const nmod_poly_t poly, const nmod_geometric_progression_t G);
-
-/*------------------------------------------------------------*/
-/* in: coeffs must have size at least d                       */
-/* out: interpolating polynomial C s.t. C(q^i) = v[i], i<d    */
-/*------------------------------------------------------------*/
-void nmod_geometric_progression_interpolate(nmod_poly_t poly, nn_srcptr v, const nmod_geometric_progression_t G);
-
-
-
 
 #ifdef __cplusplus
 }
