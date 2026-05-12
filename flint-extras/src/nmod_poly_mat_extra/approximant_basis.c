c9d5079188fff51f3ffd15cff06d19590b1ff3b3
Gilles Villard <gilles.villard@ens-lyon.fr>
2026-05-12 17:39:21 +0200

    Update flint-extras/src/nmod_poly_mat_extra/approximant_basis.c
    Co-authored-by: Vincent Neiger <vneiger@users.noreply.github.com>

--- flint-extras/src/nmod_poly_mat_extra/approximant_basis.c
+++ flint-extras/src/nmod_poly_mat_extra/approximant_basis.c
@@ -74,6 +74,7 @@ void nmod_poly_mat_pmbasis(nmod_poly_mat_t appbas,
     if (order <= PMBASIS_THRES)
     {
         nmod_poly_mat_mbasis(appbas, shift, pmat, order);
+        nmod_poly_mat_clear(pmat);
         return;
     }
 
